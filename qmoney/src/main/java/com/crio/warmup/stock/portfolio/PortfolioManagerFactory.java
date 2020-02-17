f4e955bf8779d96ef0ba0fdc5b6f39e05cc80a4e
Crio.GitLab <admin@example.com>
2020-02-17 20:30:08 +0550

    first commit

---
+++ qmoney/src/main/java/com/crio/warmup/stock/portfolio/PortfolioManagerFactory.java
@@ -0,0 +1,15 @@
+
+package com.crio.warmup.stock.portfolio;
+
+import org.springframework.web.client.RestTemplate;
+
+public class PortfolioManagerFactory {
+
+  public static PortfolioManager getPortfolioManager(RestTemplate restTemplate) {
+
+  }
+
+
+
+
+}
