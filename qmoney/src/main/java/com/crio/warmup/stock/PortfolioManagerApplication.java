25d14e50f10d972de3dfb3d187fb81d3a17cdaf7
Crio.GitLab <admin@example.com>
2020-02-17 20:30:26 +0550

    first commit

---
+++ qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
@@ -0,0 +1,81 @@
+
+package com.crio.warmup.stock;
+
+
+import com.crio.warmup.stock.dto.AnnualizedReturn;
+import com.crio.warmup.stock.dto.PortfolioTrade;
+import com.crio.warmup.stock.dto.TotalReturnsDto;
+import com.crio.warmup.stock.log.UncaughtExceptionHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.web.client.RestTemplate;
+
+
+public class PortfolioManagerApplication {
+
+
+
+
+
+
+
+  // TODO: CRIO_TASK_MODULE_REST_API
+  //  Copy the relavent code from #mainReadFile to parse the Json into PortfolioTrade list.
+  //  Now That you have the list of PortfolioTrade already populated in module#1
+  //  For each stock symbol in the portfolio trades,
+  //  Call Tiingo api (https://api.tiingo.com/tiingo/daily/<ticker>/prices?startDate=&endDate=&token=)
+  //  with
+  //   1. ticker = symbol in portfolio_trade
+  //   2. startDate = purchaseDate in portfolio_trade.
+  //   3. endDate = args[1]
+  //  Use RestTemplate#getForObject in order to call the API,
+  //  and deserialize the results in List<Candle>
+  //  Note - You may have to register on Tiingo to get the api_token.
+  //    Please refer the the module documentation for the steps.
+  //  Find out the closing price of the stock on the end_date and
+  //  return the list of all symbols in descending order by its close value on end_date
+  //  Test the function using gradle commands below
+  //   ./gradlew run --args="trades.json 2020-01-01"
+  //   ./gradlew run --args="trades.json 2019-07-01"
+  //   ./gradlew run --args="trades.json 2019-12-03"
+  //  And make sure that its printing correct results.
+
+  public static List<String> mainReadQuotes(String[] args) throws IOException, URISyntaxException {
+     return Collections.emptyList();
+  }
+
+
+
+
+
+
+
+
+
+  public static void main(String[] args) throws Exception {
+    Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler());
+    ThreadContext.put("runId", UUID.randomUUID().toString());
+
+
+    printJsonObject(mainReadQuotes(args));
+
+
+  }
+}
+
---
+++ qmoney/src/test/java/com/crio/warmup/stock/ModuleTwoTest.java
@@ -0,0 +1,76 @@
+
+package com.crio.warmup.stock;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ModuleTwoTest {
+
+  @Test
+  void mainReadQuotes() throws IOException, URISyntaxException {
+    //given
+    String filename = "assessments/trades.json";
+    List<String> expected = Arrays.asList(new String[]{"CTS", "CSCO", "MSFT"});
+
+    //when
+    List<String> actual = PortfolioManagerApplication
+        .mainReadQuotes(new String[]{filename, "2019-12-12"});
+
+    //then
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void mainReadQuotesEdgeCase() throws IOException, URISyntaxException {
+    //given
+    String filename = "assessments/empty.json";
+    List<String> expected = Arrays.asList(new String[]{});
+
+    //when
+    List<String> actual = PortfolioManagerApplication
+        .mainReadQuotes(new String[]{filename, "2019-12-12"});
+
+    //then
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void mainReadQuotesInvalidDates() throws IOException, URISyntaxException {
+    //given
+    String filename = "assessments/trades_invalid_dates.json";
+    //when
+    Assertions.assertThrows(RuntimeException.class, () -> PortfolioManagerApplication
+        .mainReadQuotes(new String[]{filename, "2017-12-12"}));
+
+  }
+
+
+  @Test
+  void mainReadQuotesInvalidStocks() throws IOException, URISyntaxException {
+    //given
+    String filename = "assessments/trades_invalid_stock.json";
+    //when
+    Assertions.assertThrows(RuntimeException.class, () -> PortfolioManagerApplication
+        .mainReadQuotes(new String[]{filename, "2017-12-12"}));
+
+  }
+
+  @Test
+  void mainReadQuotesOldTrades() throws IOException, URISyntaxException {
+    //given
+    String filename = "assessments/trades_old.json";
+    List<String> expected = Arrays.asList(new String[]{"CTS", "ABBV", "MMM"});
+
+    //when
+    List<String> actual = PortfolioManagerApplication
+        .mainReadQuotes(new String[]{filename, "2019-12-12"});
+
+    //then
+    Assertions.assertEquals(expected, actual);
+  }
+
+}
