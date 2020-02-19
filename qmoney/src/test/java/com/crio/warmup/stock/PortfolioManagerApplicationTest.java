d9758cea98ad4ce0a51ac29667fb047fd5a84a15
Crio.GitLab <admin@example.com>
2020-02-19 16:36:31 +0550

    updated changes - 2020-02-19 16:16:31

--- qmoney/src/test/java/com/crio/warmup/stock/PortfolioManagerApplicationTest.java
+++ qmoney/src/test/java/com/crio/warmup/stock/PortfolioManagerApplicationTest.java
@@ -2,8 +2,6 @@
 package com.crio.warmup.stock;
 
 import com.crio.warmup.stock.dto.PortfolioTrade;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -14,7 +12,7 @@ import org.junit.jupiter.api.Test;
 class PortfolioManagerApplicationTest {
 
   @Test
-  void mainReadFile() throws IOException, URISyntaxException {
+  void mainReadFile() throws Exception {
     //given
     String filename = "trades.json";
     List<String> expected = Arrays.asList(new String[]{"AAPL", "MSFT", "GOOGL"});
