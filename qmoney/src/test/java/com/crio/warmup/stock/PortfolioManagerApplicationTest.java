f5cbdb8280dbc6531b504c2340bc63ec22f05940
Milan097 <thummarm097@gmail.com>
2020-02-22 13:17:15 +0000

    ME_QMONEY_MODULE_JSON_PARSING

--- qmoney/src/test/java/com/crio/warmup/stock/PortfolioManagerApplicationTest.java
+++ qmoney/src/test/java/com/crio/warmup/stock/PortfolioManagerApplicationTest.java
@@ -28,22 +28,6 @@ class PortfolioManagerApplicationTest {
 
 
   @Test
-  void mainReadQuotes() throws Exception {
-    //given
-    String filename = "trades.json";
-    List<String> expected = Arrays.asList(new String[]{"MSFT", "AAPL", "GOOGL"});
-
-    //when
-    List<String> actual = PortfolioManagerApplication
-        .mainReadQuotes(new String[]{filename, "2019-12-12"});
-
-    //then
-    Assertions.assertEquals(expected, actual);
-  }
-
-
-
-  @Test
   public void testDebugValues() {
     List<String> responses = PortfolioManagerApplication.debugOutputs();
     Assertions.assertTrue(responses.get(0).contains("trades.json"));
